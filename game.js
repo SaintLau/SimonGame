2b3af6f1361c13c6b7618d776736e3c59159bcf1
Laura Santos <alsr16p@gmail.com>
2021-10-01 17:27:59 +0100

    added initialize sequence and the game mechanics

--- game.js
+++ game.js
@@ -2,14 +2,83 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
 
+let userClickedPattern = [];
+
+let started = false;
+
+let level = 0;
+
+//jQuery to detect when keyboard is pressed:
+$(document).keypress(function() {
+    if (!started) {
+        $("#level-title").text("Level " + level1);
+        nextSequence();
+        started = true;
+    }
+});
+
+//for when a button is clicked
+$("btn").click(function() {
+    let userChosenColour = $(this).attr("id"); //-> to store the id that was clicked
+    userClickedPattern.push(userChosenColour);
+   
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    //call after user clicked and chosen answer:
+    checkAnswer(userClickedPattern.length-1);
+});
+
+//check the user answer:
+function checkAnswer(currentLevel) {
+    //if-statement to check recent answer
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("sucess");
+
+    if (userClickedPattern.length === gamePattern.length) {
+        //call nextSequence after delay
+        setTimeout(function() {
+            nextSequence();
+        }, 1000);
+    }
+    } else {
+        console.log("wrong");
+    }
+}
+
 
 function nextSequence() {
-  
+    //nextSequence is trigger:
+    userClickedPattern = [];
+    
+    //everytime nextSequence is called, increase 1 level:
+    level++;
+
+    //update the h1 according to the level:
+    $("#level-title").text("Level " + level);
+
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
    
     gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100); //-> to animate the button
+
+    //attribute sound to each button
+    playSound(randomChosenColour);
+    
 }
 
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function() { //-> to remove the effect
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
 
 
